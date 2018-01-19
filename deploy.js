39477f0f3ea84c2a41a98c965d8af185c9cac937
Daniel Ignat <daniel.ignat@ica.se>
2018-01-19 15:05:30 +0100

    Read parameters passed as JSON in request body

--- deploy.js
+++ deploy.js
@@ -1,12 +1,17 @@
 // eslint-disable-next-line import/prefer-default-export
 export const trigger = (event, context, cb) => {
+  const body = JSON.parse(event.body);
+  const token = body.token;
+  const username = body.username;
+  const projectId = body.projectId;
+  const jobName = body.jobName;
   const p = new Promise((resolve) => {
     resolve('success');
   });
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Ecma Script) v1.0! Second module!',
+      message: `${body.token} - ${body.username} - ${body.projectId} - ${body.jobName}`,
       input: event,
     }),
   };
